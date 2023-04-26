50b6abd00b48e75644dcb1bbd9fa95a9f3a9dedd
Daniel Bustamante Ospina <dbustamante69@gmail.com>
2023-04-26 02:01:49 +0000

    rapid remote setup script

---
+++ setup_remote.sh
@@ -0,0 +1,5 @@
+source activate pytorch
+python --version
+pip install -r requirements.txt
+sudo snap install -y nvtop
+cp jupyter_notebook_config.py /home/ubuntu/.jupyter/jupyter_notebook_config.py
